d55317ab2858619b9e58c3718f0186b35863b68e
David Skiff <dskiff@users.noreply.github.com>
2023-08-21 02:27:05 -0400

    clean up services

--- build.sh
+++ build.sh
@@ -27,6 +27,7 @@ function build_rootfs {
                 "${rootfs}/var/lib/apt/lists" \
                 "${rootfs}/sbin"
     sudo cp -rf "${rootfs}/usr/sbin" "${rootfs}/sbin"
+    sudo mkdir "${rootfs}/overlay"
     sudo cp -rvf $ROOT_DIR/overlay/* $rootfs/
     local rootfs_img="$OUTPUT_DIR/$ROOTFS_NAME.squashfs"
     sudo mksquashfs $rootfs $rootfs_img -all-root -noappend
---
+++ overlay/usr/local/bin/entry.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+set -eu -o pipefail
+set -x
+set -m
+
+echo "INIT!"
+/bin/bash
