d21a1c0223fda600e01b11fb2186ec22696dcb43
David Skiff <dskiff@users.noreply.github.com>
2023-08-21 14:15:27 -0400

    try adding user

--- chroot.sh
+++ chroot.sh
@@ -9,3 +9,9 @@ apt update
 apt upgrade -y
 apt install -y --no-install-recommends wget curl
 apt clean
+
+# add user
+passwd -d root
+useradd -m -s /bin/bash -G sudo runner
+echo "runner:runner" | chpasswd
+echo "runner ALL=(ALL) NOPASSWD:ALL" >> /etc/sudoers
