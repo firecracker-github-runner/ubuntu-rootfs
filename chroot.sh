24eab0b621861f0a7fe83d3de98e0dec3d687511
David Skiff <dskiff@users.noreply.github.com>
2023-08-22 01:56:48 -0400

    maybe prevent resolved

--- chroot.sh
+++ chroot.sh
@@ -19,6 +19,11 @@ rm -r /home
 rm -r /media
 rm -r /root
 
+rm -f /etc/systemd/system/multi-user.target.wants/systemd-resolved.service
+rm -f /etc/systemd/system/dbus-org.freedesktop.resolve1.service
+rm -f /etc/systemd/system/sysinit.target.wants/systemd-timesyncd.service
+rm -vf /etc/systemd/system/timers.target.wants/*
+
 cat >> /etc/sysctl.conf <<EOF
 # This avoids a SPECTRE vuln
 kernel.unprivileged_bpf_disabled=1
