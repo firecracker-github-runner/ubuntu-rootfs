e9e5e3420a118c3a2bfaa9b4a6a7bd8aa72e89f1
David Skiff <dskiff@users.noreply.github.com>
2023-08-22 01:09:37 -0400

    try simple systemd networking

--- build.sh
+++ build.sh
@@ -23,7 +23,7 @@ function build_rootfs {
 
     sudo mmdebstrap \
         --arch=amd64 \
-        --include='bash,apt,sudo,dbus,locales,udev,systemd,systemd-sysv,procps,libseccomp2,curl,iproute2' \
+        --include='nano,bash,apt,sudo,dbus,locales,udev,systemd,systemd-sysv,procps,libseccomp2,curl,iproute2' \
         --variant=minbase \
         --format=dir \
         --dpkgopt='path-exclude=/usr/share/man/*' \
